b5be989865e499aac14eba418a4cb6afd1e7c9e5
Giaciii <giacun.masueger@stud.kbw.ch>
2023-07-20 15:27:29 +0200

    Bizzeli besser

--- src/php/home.php
+++ src/php/home.php
@@ -7,7 +7,7 @@ $result = $conn -> query($sql_select);
 
 if ($result -> num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-      echo $row['nachricht'];
+      echo $row['nachricht'] . "\n";
   }
 }
 ?>
--- src/php/neu.php
+++ src/php/neu.php
@@ -8,6 +8,7 @@ $sql = "INSERT INTO nachrichten (empfang, nachricht) VALUES ('$von', '$nachricht
 
 if ($conn->query($sql) === TRUE) {
   echo "Gesendet";
+  header('Location: home.php');
 } else {
   echo "Fehler: " . $sql . "<br>" . $conn->error;
 }
