2b9e27cd262f99df08871190668d3cf344b86dd9
Giaciii <giacun.masueger@stud.kbw.ch>
2023-07-20 14:25:32 +0200

    Verbindung fertig

--- src/php/conn.php
+++ src/php/conn.php
@@ -4,10 +4,8 @@
   $usr = "root";
   $pas = "";
 
-  // Create connection
   $conn = new mysqli($server, $usr, $pas, $db);
 
-  // Check connection
   if ($conn->connect_error) {
     die("Fehler: " . $conn->connect_error);
   }
