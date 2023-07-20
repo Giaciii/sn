71f531e396781fb44211a98e03a68a03d0379f6a
Giaciii <giacun.masueger@stud.kbw.ch>
2023-07-20 15:15:54 +0200

    Form für nachrichten angefangen

--- src/php/home.php
+++ src/php/home.php
@@ -1,12 +1,22 @@
 <?php
 require 'conn.php';
 
-$sql_select = "SELECT email FROM user";
+$sql_select = "SELECT * FROM nachrichten";
 
 $result = $conn -> query($sql_select);
 
 if ($result -> num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-      echo $row['email'];
+      echo $row['nachricht'];
   }
 }
+?>
+<html>
+  <body>
+    <form action="neu.php">
+      <input type="text" name="empfang" placeholder="Für">
+      <textarea type="text" name="nachricht" placeholder="Nachricht"></textarea>
+      <input type="submit" value="Senden">
+    </form>
+  </body>
+</html>
