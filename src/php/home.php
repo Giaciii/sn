126dbde85f985f07ecc69af726c9487543f0bc00
Giaciii <giacun.masueger@stud.kbw.ch>
2023-12-02 15:26:17 +0100

    Dahrstellung verbessert und Personen hinzugefügt

--- src/php/home.php
+++ src/php/home.php
@@ -13,28 +13,50 @@ if ($result -> num_rows > 0) {
   }
 }
 
-$sql_select_anfrage = "SELECT * FROM anfrage WHERE email='$uname'";
-
-$result_anfrage = $conn -> query($sql_select_anfrage);
-
-if ($result_anfrage -> num_rows > 0) {
-  while($row = $result_anfrage->fetch_assoc()) {
-    echo "Sie haben eine Anfrage von " . $row['anfrager'] . "\n";
-  }
-}
-
 ?>
 <html>
+  <head>
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
   <body>
-    <form action="anfrage.php" method="post">
-      <input type="text" name="anfrage_email" placeholder="E-Mail">
-      <input type="submit" value="Anfragen">
-    </form>
-    </form>
-    <form action="neu.php" method="post">
-      <input type="text" name="empfang" placeholder="An">
-      <textarea type="text" name="nachricht" placeholder="Nachricht"></textarea>
-      <input type="submit" value="Senden">
-    </form>
+    <div class="personen">
+
+      <?php
+        $sql_select_anfrage = "SELECT * FROM anfrage WHERE email='$uname'";
+
+        $result_anfrage = $conn -> query($sql_select_anfrage);
+        
+        if ($result_anfrage -> num_rows > 0) {
+          while($row = $result_anfrage->fetch_assoc()) {
+            echo "Sie haben eine Anfrage von " . $row['anfrager'] . "\n";
+          }
+        }
+      ?>
+
+      <form action="anfrage.php" method="post">
+        <input type="text" name="anfrage_email" placeholder="E-Mail">
+        <input type="submit" value="Anfragen">
+      </form>
+
+      <h1>Personen</h1>
+      <?php
+        $sql_select_person = "SELECT * FROM personen WHERE person1='$uname'";
+
+        $result_person = $conn -> query($sql_select_person);
+        
+        if ($result_person -> num_rows > 0) {
+          while($row = $result_person->fetch_assoc()) {
+            echo $row['person2'] . "</br>";
+          }
+        }
+      ?>
+
+    </div>  
+      </form>
+      <form action="neu.php" method="post">
+        <input type="text" name="empfang" placeholder="An">
+        <textarea type="text" name="nachricht" placeholder="Nachricht"></textarea>
+        <input type="submit" value="Senden">
+      </form>
   </body>
 </html>
